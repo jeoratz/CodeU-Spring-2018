044b36b0c1dce05f0e69d64a578af970d5e334a2
jeoratz <jeoratz@gmail.com>
2018-05-18 00:41:17 -0700

    Fixed entities

--- src/main/java/codeu/model/store/persistence/PersistentDataStore.java
+++ src/main/java/codeu/model/store/persistence/PersistentDataStore.java
@@ -68,7 +68,7 @@ public class PersistentDataStore {
     List<User> users = new ArrayList<>();
 
     // Retrieve all users from the datastore.
-    Query query = new Query("chat-users");
+    Query query = new Query("<jenna>-chat-users");
     PreparedQuery results = datastore.prepare(query);
 
     for (Entity entity : results.asIterable()) {
@@ -103,7 +103,7 @@ public class PersistentDataStore {
     List<Conversation> conversations = new ArrayList<>();
 
     // Retrieve all conversations from the datastore.
-    Query query = new Query("chat-conversations");
+    Query query = new Query("<jenna>-chat-conversations");
     PreparedQuery results = datastore.prepare(query);
 
     for (Entity entity : results.asIterable()) {
@@ -143,7 +143,7 @@ public class PersistentDataStore {
     List<Message> messages = new ArrayList<>();
 
     // Retrieve all messages from the datastore.
-    Query query = new Query("chat-messages");
+    Query query = new Query("<jenna>-chat-messages");
     PreparedQuery results = datastore.prepare(query);
 
     for (Entity entity : results.asIterable()) {
@@ -174,7 +174,7 @@ public class PersistentDataStore {
 
   public List<Event> loadEvents() throws PersistentDataStoreException {
     List<Event> events = new ArrayList<>();
-    Query query = new Query("chat-events");
+    Query query = new Query("<jenna>-chat-events");
     PreparedQuery results = datastore.prepare(query);
 
     for (Entity entity: results.asIterable()) {
@@ -238,7 +238,7 @@ public class PersistentDataStore {
     List<Profile> profiles = new ArrayList<>();
 
     // Retrieve all profiles from the datastore.
-    Query query = new Query("chat-profiles");
+    Query query = new Query("<jenna>-chat-profiles");
     PreparedQuery results = datastore.prepare(query);
 
     for (Entity entity : results.asIterable()) {
@@ -263,7 +263,7 @@ public class PersistentDataStore {
 
   /** Write a User object to the Datastore service. */
   public void writeThrough(User user) {
-    Entity userEntity = new Entity("chat-users");
+    Entity userEntity = new Entity("<jenna>-chat-users");
     userEntity.setProperty("uuid", user.getId().toString());
     userEntity.setProperty("username", user.getName());
     userEntity.setProperty("password", user.getPassword());
@@ -274,7 +274,7 @@ public class PersistentDataStore {
 
   /** Write a Message object to the Datastore service. */
   public void writeThrough(Message message) {
-    Entity messageEntity = new Entity("chat-messages");
+    Entity messageEntity = new Entity("<jenna>-chat-messages");
     messageEntity.setProperty("uuid", message.getId().toString());
     messageEntity.setProperty("conv_uuid", message.getConversationId().toString());
     messageEntity.setProperty("author_uuid", message.getAuthorId().toString());
@@ -285,7 +285,7 @@ public class PersistentDataStore {
 
   /** Write a Conversation object to the Datastore service. */
   public void writeThrough(Conversation conversation) {
-    Entity conversationEntity = new Entity("chat-conversations");
+    Entity conversationEntity = new Entity("<jenna>-chat-conversations");
     conversationEntity.setProperty("uuid", conversation.getId().toString());
     conversationEntity.setProperty("owner_uuid", conversation.getOwnerId().toString());
     conversationEntity.setProperty("title", conversation.getTitle());
@@ -297,7 +297,7 @@ public class PersistentDataStore {
   
   /** Writes an Event object to the DataStore service.*/
   public void writeThrough(Event event) {
-    Entity eventEntity = new Entity("chat-events");
+    Entity eventEntity = new Entity("<jenna>-chat-events");
     eventEntity.setProperty("creation_time", event.getTimeStamp().toString());
     eventEntity.setProperty("event_type", event.getEventType().toString());
 
@@ -340,12 +340,12 @@ public class PersistentDataStore {
   public void writeThrough(Profile profile) {
     // Delete profile in entity if already present
     Filter idFilter = new FilterPredicate("uuid", FilterOperator.EQUAL, profile.getId().toString());
-    Query query = new Query("chat-profiles").setFilter(idFilter);
+    Query query = new Query("<jenna>-chat-profiles").setFilter(idFilter);
     PreparedQuery results = datastore.prepare(query);
     for (Entity entity : results.asIterable()) {
       datastore.delete(entity.getKey());
     }
-    Entity profileEntity = new Entity("chat-profiles", profile.getId().toString());
+    Entity profileEntity = new Entity("<jenna>-chat-profiles", profile.getId().toString());
     profileEntity.setProperty("uuid", profile.getId().toString());
     profileEntity.setProperty("creation_time", profile.getCreationTime().toString());
     profileEntity.setProperty("about", profile.getAbout());
